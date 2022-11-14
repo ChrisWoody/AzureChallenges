2ae38b5032ed674a8ae845b761641436bea4d1d3
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2022-11-14 15:19:21 +0800

    First pass for retrieving and persisting state

--- src/AzureChallenges/AzureChallenges/Data/ChallengeService.cs
+++ src/AzureChallenges/AzureChallenges/Data/ChallengeService.cs
@@ -1,17 +1,41 @@
-﻿namespace AzureChallenges.Data;
+﻿using System.Text;
+
+namespace AzureChallenges.Data;
 
 public class ChallengeService
 {
-    public async Task<Challenge[]> GetResourceGroupChallenges()
+    private readonly StateService _stateService;
+
+    public ChallengeService(StateService stateService)
     {
-        return GetChallenges().Select(c => new Challenge
+        _stateService = stateService;
+    }
+
+    public async Task<Section> GetResourceGroupSection()
+    {
+        var state = await _stateService.GetState();
+
+        var sb = new StringBuilder();
+        if (!string.IsNullOrWhiteSpace(state.SubscriptionId))
         {
-            ChallengeDefinition = c,
-            Checking = false,
-            Completed = false, // populate from state
-            Error = "",
-            Input = ""
-        }).ToArray();
+            sb.Append($"https://portal.azure.com/#{"tenantname"}/resource/subscriptions/{state.SubscriptionId}");
+            if (!string.IsNullOrWhiteSpace(state.ResourceGroup))
+            {
+                sb.Append($"/resourceGroups/{state.ResourceGroup}");
+            }
+        }
+
+        return new Section
+        {
+            AzurePortalUrl = sb.ToString(),
+            Challenges = GetChallenges()
+                .Where(x => x.ResourceType == ResourceType.ResourceGroup)
+                .Select(c => new Challenge
+                {
+                    ChallengeDefinition = c,
+                    Completed = state.CompletedChallenges.Any(id => id == c.Id)
+                }).ToArray()
+        };
     }
 
     private static IEnumerable<ChallengeDefinition> GetChallenges()
@@ -20,6 +44,7 @@ public class ChallengeService
         {
             new ChallengeDefinition
             {
+                Id = Guid.Parse("ad713b6f-0f21-4889-95ee-222ef1302735"),
                 ResourceType = ResourceType.ResourceGroup,
                 Name = "Subscription",
                 Description = "Before you create the Resource Group you should determine which subscription it will live under. What is the subscription id?",
@@ -47,6 +72,7 @@ public class ChallengeService
             },
             new ChallengeDefinition
             {
+                Id = Guid.Parse("6e224d1a-40f2-48c7-bf38-05b47962cddf"),
                 ResourceType = ResourceType.ResourceGroup,
                 Name = "Create",
                 Description = "Useful for grouping Azure services together, go and create one now in the subscription you specified earlier. What is the name of the resource group you've created?",
@@ -69,22 +95,28 @@ public class ChallengeService
 
     public async Task CheckChallenge(Challenge challenge)
     {
-        await Task.Delay(1000);
+        await Task.Delay(1000); // TODO will remove, just simulating a live environment
 
         try
         {
             await challenge.ChallengeDefinition.ValidateFunc(challenge);
+            if (challenge.Completed)
+            {
+                await _stateService.ChallengeCompleted(challenge);
+            }
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
             challenge.Error = e.Message;
+            challenge.Completed = false;
         }
     }
 }
 
 public class ChallengeDefinition
 {
+    public Guid Id { get; set; }
     public ResourceType ResourceType { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
@@ -101,6 +133,12 @@ public class Challenge
     public bool Completed { get; set; }
     public string Input { get; set; }
     public string Error { get; set; }
+}
+
+public class Section
+{
+    public string AzurePortalUrl { get; set; }
+    public Challenge[] Challenges { get; set; }
 }
 
 public enum ResourceType
---
+++ src/AzureChallenges/AzureChallenges/Data/StateService.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json;
+using Azure.Storage.Blobs;
+
+namespace AzureChallenges.Data;
+
+public class StateService
+{
+    private readonly BlobContainerClient _blobContainerClient;
+
+    private StateService(BlobContainerClient blobContainerClient)
+    {
+        _blobContainerClient = blobContainerClient;
+    }
+
+    public static async Task<StateService> Create(string storageAccountConnectionString)
+    {
+        var blobServiceClient = new BlobServiceClient(storageAccountConnectionString);
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient("states");
+        await blobContainerClient.CreateIfNotExistsAsync();
+        return new StateService(blobContainerClient);
+    }
+
+    public async Task<State> GetState()
+    {
+        // TODO determine current user id/name
+        var blobClient = _blobContainerClient.GetBlobClient("someusernameorid.json");
+        if (!(await blobClient.ExistsAsync()))
+            return new State();
+
+        var content = await blobClient.DownloadContentAsync();
+        return content.Value.Content.ToObjectFromJson<State>();
+    }
+
+    public async Task SaveState(State state)
+    {
+        // TODO determine current user id/name
+        var blobClient = _blobContainerClient.GetBlobClient("someusernameorid.json");
+        var bytes = JsonSerializer.SerializeToUtf8Bytes(state);
+        await blobClient.UploadAsync(new BinaryData(bytes), overwrite: true);
+    }
+
+    public async Task ChallengeCompleted(Challenge challenge)
+    {
+        // TODO determine current user id/name
+        // TODO consider leasing
+        var state = await GetState();
+
+        state.CompletedChallenges = state.CompletedChallenges.Concat(new[] {challenge.ChallengeDefinition.Id})
+            .Distinct().Order().ToArray();
+        if (challenge.ChallengeDefinition.Id == Guid.Parse("ad713b6f-0f21-4889-95ee-222ef1302735"))
+            state.SubscriptionId = challenge.Input;
+        else if (challenge.ChallengeDefinition.Id == Guid.Parse("6e224d1a-40f2-48c7-bf38-05b47962cddf"))
+            state.ResourceGroup = challenge.Input;
+
+        await SaveState(state);
+    }
+}
+
+public class State
+{
+    public string SubscriptionId { get; set; }
+    public string ResourceGroup { get; set; }
+    public Guid[] CompletedChallenges { get; set; } = Array.Empty<Guid>();
+}
