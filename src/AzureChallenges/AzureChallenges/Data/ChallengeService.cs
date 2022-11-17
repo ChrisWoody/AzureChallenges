491c13caa7b368137e536c0df99ab78c0425cf64
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2022-11-17 12:11:11 +0800

    Add quiz type of challenge

--- src/AzureChallenges/AzureChallenges/Data/ChallengeService.cs
+++ src/AzureChallenges/AzureChallenges/Data/ChallengeService.cs
@@ -51,7 +51,7 @@ public class ChallengeService
                 Name = "Subscription",
                 Description = "Before you create the Resource Group you should determine which subscription it will live under. What is the subscription id?",
                 Hint = "It's best to use a 'development' subscription, this means you'll have access to create/update resources and benefit from dev/test pricing.",
-                RequiresInput = true,
+                ChallengeType = ChallengeType.ExistsWithInput,
                 ValidateFunc = async c =>
                 {
                     if (!string.IsNullOrWhiteSpace(c.Input) && Guid.TryParse(c.Input, out _))
@@ -77,7 +77,7 @@ public class ChallengeService
                 ResourceType = ResourceType.ResourceGroup,
                 Name = "Create",
                 Description = "Useful for grouping Azure services together, go and create one now in the subscription you specified earlier. What is the name of the resource group you've created?",
-                RequiresInput = true,
+                ChallengeType = ChallengeType.ExistsWithInput,
                 ValidateFunc = async c =>
                 {
                     var state = await _stateService.GetState();
@@ -89,6 +89,25 @@ public class ChallengeService
                     {
                         c.Error = $"Could not find resource group '{c.Input}'.";
                     }
+                }
+            },
+            new ChallengeDefinition
+            {
+                Id = Guid.Parse("23aca336-d1c4-4b66-806c-cddb6629f5a0"),
+                ResourceType = ResourceType.ResourceGroup,
+                Name = "Quiz",
+                Description = "Subscriptions are a level above Resource Groups, whats the level above a Subscription?",
+                ChallengeType = ChallengeType.Quiz,
+                QuizOptions = new []
+                {
+                    "Storage Account", "Management Group", "Tenant", "The sky"
+                },
+                ValidateFunc = async c =>
+                {
+                    if (c.Input == "Tenant")
+                        c.Completed = true;
+                    else
+                        c.Error = "Sorry that's not correct";
                 }
             }
         };
@@ -125,8 +144,9 @@ public class ChallengeDefinition
     public string Name { get; set; }
     public string Description { get; set; }
     public string Hint { get; set; }
-    public bool RequiresInput { get; set; }
     public Func<Challenge, Task> ValidateFunc { get; set; }
+    public ChallengeType ChallengeType { get; set; }
+    public string[] QuizOptions { get; set; }
 
 }
 
@@ -149,4 +169,11 @@ public enum ResourceType
 {
     ResourceGroup,
     StorageAccount
+}
+
+public enum ChallengeType
+{
+    ExistsWithInput,
+    CheckConfigured,
+    Quiz
 }
